7e0139a8c0307f172667d8831c83f72a23eed726
Julien Fastré <julien@fastre.info>
2013-10-26 13:19:42 +0200

    Add Country

---
+++ Entity/Country.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace CL\Chill\MainBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Country
+ */
+class Country
+{
+    /**
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $label;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set label
+     *
+     * @param string $label
+     * @return Country
+     */
+    public function setLabel($label)
+    {
+        $this->label = $label;
+    
+        return $this;
+    }
+
+    /**
+     * Get label
+     *
+     * @return string 
+     */
+    public function getLabel()
+    {
+        return $this->label;
+    }
+}

d3a0b2b82b7e0f4195a535139e4367f973a0ed66
Julien Fastré <julien@fastre.info>
2013-10-26 11:38:21 +0200

    default layout quite advanced !

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -8,6 +8,22 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
+        $a = array(
+            array('success', "Message de succès ! "),
+            array('success',"Un autre message de succès ! "),
+            array('danger' , "Avert lorem ipsum dolor sit amet spiritur"),
+            array('danger' ,"Avertum ipsut est amet amergitur tatouari"),
+            array('info' , "Un message d'information s'affiche."),
+            array('info' , "Info informitur escept asolitur amet. Sit Roberetur astot.")
+        );
+        
+        if ($this->getRequest()->query->get('addMessages', 1)) {
+            foreach ($a as $array) {
+                $this->get('session')->getFlashBag()->add($array[0], $array[1]);
+            }
+        }
+        
+        
         return $this->render('CLChillMainBundle::layout.html.twig');
     }
 }

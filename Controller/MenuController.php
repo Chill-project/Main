5393473c11021db9c33ea98a8e5e773de83edd5f
Julien Fastré <julien@fastre.info>
2013-11-06 17:46:43 +0100

    Create a controller for rendering menu

---
+++ Controller/MenuController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace CL\Chill\MainBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class MenuController extends Controller
+{
+    public function writeMenuAction($menu, $layout, $activeRouteKey, array $args = array() )
+    {
+        
+        
+        
+        return $this->render($layout, array(
+            'menu_composer' => $this->get('cl_chill_main_menu_composer'),
+            'menu' => $menu,
+            'args' => $args,
+            'activeRouteKey' => $activeRouteKey
+        ));
+    }
+}

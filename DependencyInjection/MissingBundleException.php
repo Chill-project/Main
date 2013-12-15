33465a34902500cd61743c2bc05a070fd3cc1d54
Julien Fastré <julien@fastre.info>
2013-12-15 23:33:34 +0100

    add base configuration for user

---
+++ DependencyInjection/MissingBundleException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace CL\Chill\MainBundle\DependencyInjection;
+
+use Exception;
+
+/**
+ * Description of MissingBundleException
+ *
+ * @author julien
+ */
+class MissingBundleException extends Exception {
+    
+    public function __construct($missingBundleName) {
+        $message = "The bundle $missingBundleName is missing.";
+        
+        parent::__construct($message, 500);
+    }
+    
+}
---
+++ Entity/Agent.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace CL\Chill\MainBundle\Entity;
+
+use FOS\UserBundle\Entity\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Agent
+ */
+class Agent extends BaseUser {
+    
+    /**
+     * @var integer
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+    
+    public function __construct() {
+        parent::__construct();
+    }
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     * @return Agent
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+    
+    public function __toString() {
+        parent::__toString();
+    }
+}

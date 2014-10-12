c91a667c70e3ee726d2035a2b56d727daf8c58e8
Julien Fastré <julien@fastre.info>
2014-10-12 20:38:26 +0200

    add chill_menu to render easily menu, refs #179

---
+++ Routing/MenuTwig.php
@@ -0,0 +1,107 @@
+<?php
+
+/**
+ * 
+ * Copyright (C) 2014 Champs Libres Cooperative SCRLFS, 
+ * <http://www.champs-libres.coop>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace CL\Chill\MainBundle\Routing;
+
+use CL\Chill\MainBundle\Routing\MenuComposer;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Add the filter 'chill_menu'
+ *
+ * @author Julien Fastré <julien arobase fastre point info>
+ */
+class MenuTwig extends \Twig_Extension implements ContainerAwareInterface
+{
+    
+    /**
+     *
+     * @var MenuComposer
+     */
+    private $menuComposer;
+    
+    /**
+     *
+     * @var \Symfony\Component\DependencyInjection\ContainerInterface
+     */
+    private $container;
+    
+    /**
+     * the default parameters for chillMenu
+     * 
+     * @var mixed[] 
+     */
+    private $defaultParams = array(
+        'layout' => 'CLChillMainBundle:Menu:defaultMenu.html.twig',
+        'args' => array(),
+        'activeRouteKey' => null
+    );
+    
+    public function __construct(MenuComposer $menuComposer)
+    {
+        $this->menuComposer = $menuComposer;
+    }
+    
+    public function getFunctions()
+    {
+        return [new \Twig_SimpleFunction('chill_menu', 
+                array($this, 'chillMenu'), array('is_safe' => array('html')))
+            ];
+    }
+    
+    /**
+     * Render a Menu corresponding to $menuId
+     * 
+     * Expected params : 
+     * - args: the arguments to build the path (i.e: if pattern is /something/{bar}, args must contain {'bar': 'foo'}
+     * - layout: the layout. Absolute path needed (i.e.: CLChillXyzBundle:section:foo.html.twig)
+     * - activeRouteKey : the key active, will render the menu differently.
+     * 
+     * see https://redmine.champs-libres.coop/issues/179 for more informations
+     * 
+     * @param string $menuId
+     * @param mixed[] $params
+     */
+    public function chillMenu($menuId, array $params = array())
+    {
+        $resolvedParams = array_merge($this->defaultParams, $params);
+
+        $layout = $resolvedParams['layout'];
+        unset($resolvedParams['layout']);
+        
+        $resolvedParams['routes'] = $this->menuComposer->getRoutesFor($menuId);
+        
+        return $this->container->get('templating')
+                ->render($layout, $resolvedParams);
+    }
+    
+    public function getName()
+    {
+        return 'chill_menu';
+    }
+
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
+
+}
---
+++ Tests/Services/ChillMenuTwigFunctionTest.php
@@ -0,0 +1,86 @@
+<?php
+
+/*
+ * <one line to give the program's name and a brief idea of what it does.>
+ * Copyright (C) 2014, Champs Libres Cooperative SCRLFS, <http://www.champs-libres.coop>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace CL\Chill\MainBundle\Tests\Services;
+
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\DomCrawler\Crawler;
+
+/**
+ * Test the Twig function 'chill_menu'
+ *
+ * @author Julien Fastré <julien.fastre@champs-libres.coop>
+ */
+class ChillMenuTwigFunctionTest extends KernelTestCase
+{
+    
+    private static $templating;
+    
+    public static function setUpBeforeClass()
+    {
+        self::bootKernel(array('environment' => 'test'));
+        static::$templating = static::$kernel
+                ->getContainer()->get('templating');
+        //load templates in Tests/Resources/views
+        static::$kernel->getContainer()->get('twig.loader')
+                ->addPath(__DIR__.'/../Fixtures/Resources/views/', $namespace = 'tests');
+    }
+    
+    public function testNormalMenu()
+    {
+        $content = static::$templating->render('@tests/menus/normalMenu.html.twig');
+        $crawler = new Crawler($content);
+        
+        $ul = $crawler->filter('ul')->getNode(0);
+        $this->assertEquals( 'ul', $ul->tagName);
+        
+        $lis = $crawler->filter('ul')->children();
+        $this->assertEquals(3, count($lis));
+        
+        $lis->each(function(Crawler $node, $i) {
+                $this->assertEquals('li', $node->getNode(0)->tagName);
+                
+                $a = $node->children()->getNode(0);
+                $this->assertEquals('a', $a->tagName);
+                switch($i) {
+                    case 0: 
+                        $this->assertEquals('/dummy?param=fake', $a->getAttribute('href'));
+                        $this->assertEquals('active', $a->getAttribute('class'));
+                        $this->assertEquals('test0', $a->nodeValue);
+                        break;
+                    case 1:
+                        $this->assertEquals('/dummy1?param=fake', $a->getAttribute('href'));
+                        $this->assertEmpty($a->getAttribute('class'));
+                        $this->assertEquals('test1', $a->nodeValue);
+                        break;
+                    case 3:
+                        $this->assertEquals('/dummy2/fake', $a->getAttribute('href'));
+                        $this->assertEmpty($a->getAttribute('class'));
+                        $this->assertEquals('test2', $a->nodeValue);
+                }
+        });
+    }
+    
+    public function testMenuOverrideTemplate()
+    {
+        $content = static::$templating->render('@tests/menus/overrideTemplate.html.twig');
+        $this->assertEquals('fake template', $content);
+    }
+}

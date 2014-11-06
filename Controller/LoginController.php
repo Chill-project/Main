cbbf2e0524f3774856114290eb260e672b80c7ca
Julien Fastré <julien@fastre.info>
2014-11-06 16:37:30 +0100

    login form

---
+++ Controller/LoginController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Chill\MainBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\SecurityContextInterface;
+
+class LoginController extends Controller
+{
+    /**
+     * 
+     * @todo Improve this with http://symfony.com/blog/new-in-symfony-2-6-security-component-improvements#added-a-security-error-helper
+     * @param Request $request
+     * @return Response
+     */
+    public function loginAction(Request $request)
+    {
+        
+        $session = $request->getSession();
+
+        if ($request->attributes->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(
+                SecurityContextInterface::AUTHENTICATION_ERROR
+            );
+        } elseif (null !== $session && $session->has(SecurityContextInterface::AUTHENTICATION_ERROR)) {
+            $error = $session->get(SecurityContextInterface::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContextInterface::AUTHENTICATION_ERROR);
+        } else {
+            $error = '';
+        }
+
+        $lastUsername = (null === $session) ? 
+              '' : $session->get(SecurityContextInterface::LAST_USERNAME);
+
+
+        return $this->render('ChillMainBundle:Login:login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error
+        ));  
+        
+    }
+    
+    public function LoginCheckAction(Request $request)
+    {
+        
+    }
+
+}
---
+++ Tests/Controller/LoginControllerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Chill\MainBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class LoginControllerTest extends WebTestCase
+{
+    public function testLogin()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/login');
+    }
+
+}

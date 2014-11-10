3f214da596ea19662c8284bfcb60f5c96da4923c
Marc Ducobu <marc@champs-libres.coop>
2014-11-10 11:18:10 +0100

    config cl_chill_main -> chill_main

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('cl_chill_main');
+        $rootNode = $treeBuilder->root('chill_main');
 
         $rootNode
             ->children()

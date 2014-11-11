6642edb3e90941f42c662f57e5942e1c774c611c
Marc Ducobu <marc@champs-libres.coop>
2014-11-11 16:56:20 +0100

    Improving navigation

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -7,23 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     public function indexAction()
-    {
-        $a = array(
-            array('success', "Message de succès (success)! "),
-            array('success',"Un autre message de succès ! "),
-            array('danger' , "(danger)Avert lorem ipsum dolor sit amet spiritur"),
-            array('danger' ,"Avertum ipsut est amet amergitur tatouari"),
-            array('info' , "(info) Un message d'information s'affiche."),
-            array('info' , "Info informitur escept asolitur amet. Sit Roberetur astot.")
-        );
-        
-        if ($this->getRequest()->query->get('addMessages', 1)) {
-            foreach ($a as $array) {
-                $this->get('session')->getFlashBag()->add($array[0], $array[1]);
-            }
-        }
-        
-        
+    {        
         return $this->render('ChillMainBundle::layout.html.twig');
     }
 }

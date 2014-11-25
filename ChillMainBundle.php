4fbe254d346d216af7cc28e709b97edda92f4fbc
Julien Fastré <julien@fastre.info>
2014-11-25 14:49:15 +0100

    add search possibility throught all bundles, and remove deps between mainbundle and person bundle.
    refs #223

--- ChillMainBundle.php
+++ ChillMainBundle.php
@@ -3,7 +3,14 @@
 namespace Chill\MainBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Chill\MainBundle\DependencyInjection\SearchableServicesCompilerPass;
 
 class ChillMainBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $container->addCompilerPass(new SearchableServicesCompilerPass());
+    }
 }

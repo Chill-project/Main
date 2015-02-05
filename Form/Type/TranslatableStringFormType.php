caaab740a9b77384a38783426444a7dee7c47eb8
Julien Fastré <julien@fastre.info>
2015-02-05 16:31:33 +0100

    remove ref to %locale% in translatable form

--- Form/Type/TranslatableStringFormType.php
+++ Form/Type/TranslatableStringFormType.php
@@ -8,21 +8,23 @@ namespace Chill\MainBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Translation\Translator;
 
 class TranslatableStringFormType extends AbstractType
 {
     private $availableLanguages;  // The langauges availaible
     private $frameworkTranslatorFallback; // The langagues used for the translation
 
-    public function __construct(array $availableLanguages, $frameworkTranslatorFallback) {
+    public function __construct(array $availableLanguages, Translator $translator) {
         $this->availableLanguages = $availableLanguages;
-        $this->frameworkTranslatorFallback = $frameworkTranslatorFallback;
+        $this->frameworkTranslatorFallback = $translator->getFallbackLocales();
     }
 
     public function buildForm(FormBuilderInterface $builder, array $options) {
         foreach ($this->availableLanguages as $lang) {
             $builder->add($lang, 'text',
-                array('required' => ($lang === $this->frameworkTranslatorFallback)));
+                array('required' => (in_array($lang, 
+                      $this->frameworkTranslatorFallback))));
         }
     }
 

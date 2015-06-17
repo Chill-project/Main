9ad9f624a0aa73ca2e639fd68fe4d5559da2bbd7
Julien Fastré <julien.fastre@champs-libres.coop>
2015-06-18 01:01:16 +0200

    add utilities to generate prophesized entities
    entities
    - User (with permissions)
    - Center
    - Scope
    may be  generated by trait/methods
    
    This ease test writing about acl

---
+++ Test/PrepareScopeTrait.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * Copyright (C) 2015 Julien Fastré <julien.fastre@champs-libres.coop>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Chill\MainBundle\Test;
+
+/**
+ * A trait to prepare center
+ * 
+ * **Usage :** You must set up trait with `setUpTrait` before use
+ * and use tearDownTrait after usage.
+ * 
+ *
+ * @author Julien Fastré <julien.fastre@champs-libres.coop>
+ */
+trait PrepareScopeTrait
+{
+    use ProphecyTrait;
+    
+    /**
+     * prepare a mocked center, with and id and name given
+     * 
+     * @param int $id
+     * @param string $name
+     * @return \Chill\MainBundle\Entity\Center 
+     */
+    protected function prepareScope($id, $name)
+    {
+        if ($this->prophet === NULL) {
+            throw new \LogicException('You must set up trait using the `setUpTrait`'
+                    . ' before any invocation');
+        }
+        
+        $scope = $this->prophet->prophesize();
+        $scope->willExtend('\Chill\MainBundle\Entity\Scope');
+        $scope->getId()->willReturn($id);
+        $scope->getName()->willReturn($name);
+        
+        return $scope->reveal();
+    }
+}
---
+++ Test/ProphecyTrait.php
@@ -0,0 +1,49 @@
+<?php
+
+/*
+ * Copyright (C) 2015 Julien Fastré <julien.fastre@champs-libres.coop>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Chill\MainBundle\Test;
+
+/**
+ * a trait to prepare prophecy
+ * 
+ * **Usage : ** You must set up trait with `setUpTrait` before use
+ * and use tearDownTrait after usage.
+ *
+ * @author Julien Fastré <julien.fastre@champs-libres.coop>
+ */
+trait ProphecyTrait
+{
+        
+    protected $prophet;
+    
+    public function setUpTrait()
+    {
+        $this->prophet = new \Prophecy\Prophet();
+    }
+    
+    public function tearDownTrait()
+    {
+        $this->prophet->checkPredictions();
+    }
+    
+    abstract public function setUp();
+    
+    abstract public function tearDown();
+    
+}

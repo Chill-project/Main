38c8a60f56e0870f5c196daa7e1d6851ea6edb8e
Julien Fastré <julien.fastre@champs-libres.coop>
2015-08-25 16:19:46 +0200

    add autogenerated user entity
    [ci-skip]

---
+++ Form/UserType.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Chill\MainBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('password')
+            ->add('salt')
+            ->add('enabled')
+            ->add('locked')
+            ->add('groupCenters')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Chill\MainBundle\Entity\User'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'chill_mainbundle_user';
+    }
+}

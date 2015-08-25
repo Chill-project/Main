eb67639673543f8153308b2fa863047ec0eeddc3
Julien Fastré <julien.fastre@champs-libres.coop>
2015-08-25 16:21:01 +0200

    add autogenerated crud for entity permissionGroup
    [ci-skip]

---
+++ Form/PermissionsGroupType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Chill\MainBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class PermissionsGroupType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('roleScopes')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Chill\MainBundle\Entity\PermissionsGroup'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'chill_mainbundle_permissionsgroup';
+    }
+}
